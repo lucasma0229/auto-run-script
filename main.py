cfbf1a9a296004c530ce687941a3f76945a35a3c
lucasma0229 <lucasma0229@gmail.com>
2026-01-22 23:36:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-from datetime import datetime
-import random
-
-now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
-print("Hello from GitHub Actions!")
-print("Time:", now)
-print("Random:", random.randint(1, 1000000))
+print("GitHub 自动运行成功 🚀")
